70098d88684846c73a5f1a0988ba72f3209a6721
Armaan Swan <swanarmaan@gmail.com>
2024-06-08 15:19:54 -0700

    Add activity messages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-const nameHeading = document.getElementById('name');
-const bottomSection = document.getElementById('bottom-section');
+document.addEventListener('DOMContentLoaded', function () {
+    const nameHeading = document.getElementById('name');
+    const bottomSection = document.getElementById('bottom-section');
 
-bottomSection.style.width = `${nameHeading.offsetWidth}px`;
+    bottomSection.style.width = `${nameHeading.offsetWidth}px`;
+
+    // Activities List Item Hover Effect
+    const items = document.querySelectorAll('.activities li');
+    const messageContainer = document.getElementById('activity-message-container');
+
+    items.forEach(item => {
+        item.addEventListener('mouseover', function () {
+            let text = item.getAttribute('data-text');
+            text = text.split('\\n').join('\n');
+            messageContainer.textContent = text;
+        });
+
+        item.addEventListener('mouseout', function () {
+            messageContainer.textContent = '';
+        });
+    });
+});
